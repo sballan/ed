7bd0541e4727488bc74107a0f73f5ad9b46d33fa
Samuel Ballan <sgb4622@gmail.com>
2019-03-22 21:44:28 -0400

    session, current user in application controller, setup routes for users and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "You've logged in!"
+    else
+      # Use now for this flash to only last for this action
+      flash.now.alert = "Failed to log in"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "You've logged out!"
   end
 end
